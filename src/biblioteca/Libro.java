f76dcaecf6fa69b79e4bafed7a75cd8676d9facb
Gonzalo Robles <gonrobles@gmail.com>
2022-07-02 12:48:01 -0400

    Primera version

---
+++ src/biblioteca/Libro.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package biblioteca;
+
+/**
+ *
+ * @author gonro
+ */
+public class Libro {
+    private int codigoL;
+    private String titulo;
+    private String autor;
+    
+    
+}
