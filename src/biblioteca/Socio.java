dfb2b58e0f579ab6f16578f4632ab9f718f1d229
Gonzalo Robles <gonrobles@gmail.com>
2022-07-03 22:30:55 -0400

    Primera version

---
+++ src/biblioteca/Socio.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package biblioteca;
+
+/**
+ *
+ * @author gonro
+ */
+public class Socio {
+    private int rut;
+    private String nombre;
+    private String direccion;
+
+    public Socio(int rut, String nombre, String direccion) {
+        this.rut = rut;
+        this.nombre = nombre;
+        this.direccion = direccion;
+    }
+
+    public int getRut() {
+        return rut;
+    }
+
+    public void setRut(int rut) {
+        this.rut = rut;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+    
+
+}
